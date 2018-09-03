495481514b357023d5be79ecf11999973451805a
s-triar <s.triar@live.com>
2018-09-03 13:42:29 +0700

    add commend

--- createFolder.py
+++ createFolder.py
@@ -1,10 +1,10 @@
 import os
-n = 32
-path = "D:\\FOTO DATA SKRIPSI\\"
+n = 32  # number sub folder
+path = "D:\\FOTO DATA SKRIPSI\\"  # path to folder data
 for i in range(n):
     t = i+1
-    r = os.path.join(path, '2'+str(t))
+    r = os.path.join(path, '0'+str(t))
     if t < 10:
-        r = os.path.join(path, '20'+str(t))
+        r = os.path.join(path, '00'+str(t))
     if not os.path.exists(r):
         os.makedirs(r)
